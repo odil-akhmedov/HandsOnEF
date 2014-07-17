3cd199a56897dae0723fe3a448f381d21a517de7
odil-akhmedov <akhmedoff.odil@gmail.com>
2014-07-17 13:38:41 -0400

    Creating Models

---
+++ HandsOnEF/HandsOnEF/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HandsOnEF.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string First { get; set; }
+        public string Las { get; set; }
+        public int PositionId { get; set; }
+        public int DepartmentId { get; set; }
+
+        public virtual Department Department { get; set; }
+
+    }
+}
---
+++ HandsOnEF/HandsOnEF/Models/Timesheet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HandsOnEF.Models
+{
+    public class Timesheet
+    {
+        public int TimesheetId { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime TimeIn { get; set; }
+        public DateTime TimeOut { get; set; }
+        //Navigation properties
+        public virtual Employee Employee { get; set; }
+
+    }
+}
